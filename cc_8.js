ecbb0b71c065892d737b1ca1f081a49403c5b970
aliciariveraa <aliciarivera@usf.edu>
2025-02-16 17:57:08 -0500

    Task 6 - Identifying Large Transactions

--- cc_8.js
+++ cc_8.js
@@ -75,3 +75,16 @@ function calculateLoanPayment(principal, rate, time) {
 // Test Data
 console.log(calculateLoanPayment(1000, 0.05, 2)); // Total Payment: $1100.00
 console.log(calculateLoanPayment(5000, 0.07, 3)); // Total Payment: $6050.00
+
+
+
+// Task 6 - Identifying Large Transactions
+
+let transactions = [200, 1500, 3200, 800, 2500];
+
+function filterLargeTransactions(transactions, filterFunction) {
+    return transactions.filter(filterFunction);
+}
+
+// Test Data
+console.log(filterLargeTransactions(transactions, amount => amount > 1000)); // [1500, 3200, 2500]
