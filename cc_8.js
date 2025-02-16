98cd2e66e849e496ef7347fc37057b06c3810752
aliciariveraa <aliciarivera@usf.edu>
2025-02-16 17:48:16 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,16 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 // Test Data
 console.log(calculateSalary(5000, 500, 0.1)); // Net Salary: $5000.00
 console.log(calculateSalary(7000, 1000, 0.15)); // Net Salary: $6950.00
+
+
+
+// Task 2 - Product Price After Discount
+
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+}
+
+// Test Data
+console.log(calculateDiscount(100, 0.2)); // Final Price: $80.00
+console.log(calculateDiscount(250, 0.15)); // Final Price: $212.50
