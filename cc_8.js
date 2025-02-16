cfd358598dcc58b9b5291bc2f1da0a48839c192a
aliciariveraa <aliciarivera@usf.edu>
2025-02-16 17:59:11 -0500

    Task 8 - Savings Growth Projection

--- cc_8.js
+++ cc_8.js
@@ -104,3 +104,17 @@ function createCartTracker() {
 let cart = createCartTracker();
 console.log(cart(20)); // Total Cart Value: $20
 console.log(cart(35)); // Total Cart Value: $55
+
+
+
+// Task 8 - Savings Growth Projection
+
+function calculateSavings(years, amount) {
+    if (years === 0) return `Projected Savings: $${amount.toFixed(2)}`;
+    amount += amount * 0.05;
+    return calculateSavings(years - 1, amount);
+}
+
+// Test Data
+console.log(calculateSavings(8, 1000)); // Projected Savings: $1102.50
+console.log(calculateSavings(5, 5000)); // Projected Savings: $6381.41
